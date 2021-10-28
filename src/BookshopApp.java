187251bc0d413b5d898e0f9491f89f19be17d5c7
onionmccabbage <onionmccabbage@live.com>
2021-10-28 09:24:17 +0100

    Delete BookshopApp.java

--- src/BookshopApp.java
+++
@@ -1,9 +0,0 @@
-/**
- * Simple Hello World style App
- */
-public class BookshopApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Java Bookshop");
-        System.out.println("============================");
-    }
-}
